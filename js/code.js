b963a497133e7ddc89888cf59c85a0b4966f89aa
Pedro Ivo <pi3virgula14@outlook.com>
2018-08-20 15:02:26 -0300

    Adicionada animação no background e menu

--- js/code.js
+++ js/code.js
@@ -1,74 +1,111 @@
-//Checks
-$(document).ready(function() {
-    if(getCookie("backgroundPosition") !== "" && window.scrollY !== 0) document.getElementById("slide").style.backgroundPosition = getCookie("backgroundPosition");
-});
 //Eventos
 //Evento da HASH
 window.addEventListener("hashchange", menuClick);
 
 //Quando der scroll
+let lastScroll = window.scrollY;
 window.onscroll = function(){
-    var background = document.getElementById("slide");
-    if(window.scrollY >= document.querySelector(".processoseletivo-section").offsetTop - window.innerHeight + $(".processoseletivo-section").height()){
-        background.
-    }
+    changeSlideScroll();
+    backPosChange();
+    menuBarChange();
 };
-
-//Eventos quando o mouse vai para cime ou para baixo
-window.addEventListener('wheel', function(e){
-    if(e.deltaY < 0){
-        backgroundMove.toUp();
-    }if(e.deltaY > 0){
-        backgroundMove.toBottom();
-    }
-});
 
 //Quando clicar no menu
 function menuClick(){
     let deskButton = document.getElementsByClassName("desk-menu-button");
     for(let i = 0; i < deskButton.length; i++){
-        if("#" + deskButton.href == window.location.hash){
+        if("#" + deskButton.href === window.location.hash){
 
         }
     }
 }
 
-//classes
+//Quando mudar o slide
+function changeSlideScroll(){
+    let actualBack = 1;
+    if(window.scrollY >= elemGetPosition(".solucoes-section", 2)){
+        if (actualBack !== 2) background.change(2);
+    }else if(window.scrollY <= elemGetPosition(".solucoes-section", 1)){
+        if (actualBack !== 1) background.change(1);
+    }
+}
 
-//Fazer o background se mover
-class backgroundMove{
+//Mudar posição do slide
+function backPosChange(){
+    if(lastScroll > window.scrollY){ // Descendo
+        background.toBottom();
+    }else if(lastScroll < window.scrollY){ // Subindo
+        background.toUp();
+    }
+    lastScroll = window.scrollY;
+}
+
+//Mudar a barra do menu
+function menuBarChange(){
+    const scrollPos = window.scrollY;
+    let menuBtClass = document.getElementsByClassName("desk-menu-button");
+    if(scrollPos >= 440){
+        document.querySelector("header").classList.add("menuScrolled");
+        for(let i = 0; i < menuBtClass.length; i++) menuBtClass[i].classList.add("desk-menu-button-scrolled");
+    }else{
+        document.querySelector("header").classList.remove("menuScrolled");
+        for(let i = 0; i < menuBtClass.length; i++) menuBtClass[i].classList.remove("desk-menu-button-scrolled");
+    }
+}
+
+/*Checar posição do elemento*/
+function elemGetPosition(element, orientation){
+    const elem = document.querySelector(element);
+    if(orientation === 1){
+        return elem.offsetTop - window.innerHeight + $(element).height();
+    }else if(orientation === 2){
+        return elem.offsetTop;
+    }
+}
+
+/*
+classes
+
+Fazer o background se mover
+*/
+class background{
     static toUp() {
-        var element = document.getElementById("slide");
-        var positionValue = parseInt(document.getElementById("slide").style.backgroundPosition.substring(3, 6));
+        const element = document.getElementById("slide");
+        const positionValue = parseInt(document.getElementById("slide").style.backgroundPosition.substring(3, 6));
         if (positionValue >= 0) element.style.backgroundPosition = "0 " + (positionValue - 1) + "%";
     }
 
     static toBottom(){
-        var element = document.getElementById("slide");
-        var positionValue = parseInt(document.getElementById("slide").style.backgroundPosition.substring(3,6));
+        const element = document.getElementById("slide");
+        const positionValue = parseInt(document.getElementById("slide").style.backgroundPosition.substring(3, 6));
         if(positionValue < 51) element.style.backgroundPosition = "0 " + (positionValue + 1) + "%";
+    }
 
+    static change(id){
+        let background = document.getElementById("slide");
+        background.class = "";
+        background.class = "slide slide" + id;
     }
 }
 
 
 //COOKIES
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
+    const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
+    const expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
+    const name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
---
+++ readme.md
@@ -0,0 +1,7 @@
+Copyright MyProject \
+Author: Pedro Ivo
+
+Site para empresa Júnior da UNIFOR (Universidade de Fortaleza)
+
+Status: \
+Desenvolvimento...
